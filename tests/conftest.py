958cad592a91150d15ae6a07045530f9fc908d0c
vgeorgescu <vgeorgescu@gmail.com>
2018-12-30 23:02:06 +0200

    Add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from myapp import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
+    app.config['SERVER_NAME'] = 'localhost.localdomain'
     client = app.test_client()
 
     return client
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,4 +1,15 @@
+from flask import url_for
+from myapp import app
+
 def test_home(client):
     """Check the home page"""
     response = client.get('/')
     assert b'Hello, world!' in response.data
+
+
+def test_dashboard_redirect(client):
+    """Check that dashboard redirects to login when unauthenticated"""
+    with app.app_context():
+        response = client.get('/dashboard')
+        assert response.status_code == 302
+        assert response.location == url_for('auth.login', _external=True)
